088831a9009e501ac0ed13c94b68e52ced461e7d
sani.usnain <sani.usnain@onescreensolutions.com>
2024-01-25 14:37:00 +0500

    added completed key in model, added complete, uncomplete todos route method

--- model/TodoModel.js
+++ model/TodoModel.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const todoSchema = mongoose.Schema({
     text: { type: "string" },
+    completed: { 
+        type: "boolean",
+        default: false
+    },
 });
 
 module.exports = mongoose.model("Todo", todoSchema);
--- routes/todo-route.js
+++ routes/todo-route.js
@@ -5,18 +5,11 @@ const Todo = require('../model/TodoModel');
 router.post('/', async (req, res, next) => {
 	try {
 		const { text } = req.body;
- 
-		// Validate input
 		if (!text) {
 			return res.status(400).json({ error: 'Text is required for a todo.' });
 		}
-
-		// Create a new todo
 		const newTodo = new Todo({ text });
-
-		// Save the todo to the database
 		await newTodo.save();
-
 		res.status(201).json({ message: 'Todo added successfully.' });
 	} catch (error) {
 		console.error(error);
@@ -26,9 +19,7 @@ router.post('/', async (req, res, next) => {
 
 router.get('/', async (req, res) => {
 	try {
-		// Fetch all todos from the database
 		const allTodos = await Todo.find().sort({ _id: -1 });
-
 		res.status(200).json(allTodos);
 	} catch (error) {
 		console.error(error);
@@ -38,9 +29,7 @@ router.get('/', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
 	try {
-		// Fetch all todos from the database
 		const allTodos = await Todo.deleteOne({_id: req.params.id})
-
 		res.status(200).json(allTodos);
 	} catch (error) {
 		console.error(error);
@@ -52,7 +41,17 @@ router.put('/:id', async (req, res) => {
 	try {
 		const {text} = req.body;
 		const allTodos = await Todo.findOneAndUpdate({_id: req.params.id}, {text: text});
+		res.status(200).json(allTodos);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+});
 
+router.put('/:id/:status', async (req, res) => {
+	try {
+		const {id, status} = req.params;
+		const allTodos = await Todo.findOneAndUpdate({_id: id}, {completed: status});
 		res.status(200).json(allTodos);
 	} catch (error) {
 		console.error(error);
