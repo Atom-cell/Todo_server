876f306393ee05c9af192fef3faaa521867ad06b
sani.usnain <sani.usnain@onescreensolutions.com>
2024-01-25 12:28:48 +0500

    added delete and put routes

--- routes/todo-route.js
+++ routes/todo-route.js
@@ -5,7 +5,7 @@ const Todo = require('../model/TodoModel');
 router.post('/', async (req, res, next) => {
 	try {
 		const { text } = req.body;
-
+ 
 		// Validate input
 		if (!text) {
 			return res.status(400).json({ error: 'Text is required for a todo.' });
@@ -27,7 +27,31 @@ router.post('/', async (req, res, next) => {
 router.get('/', async (req, res) => {
 	try {
 		// Fetch all todos from the database
-		const allTodos = await Todo.find();
+		const allTodos = await Todo.find().sort({ _id: -1 });
+
+		res.status(200).json(allTodos);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+});
+
+router.delete('/:id', async (req, res) => {
+	try {
+		// Fetch all todos from the database
+		const allTodos = await Todo.deleteOne({_id: req.params.id})
+
+		res.status(200).json(allTodos);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+});
+
+router.put('/:id', async (req, res) => {
+	try {
+		const {text} = req.body;
+		const allTodos = await Todo.findOneAndUpdate({_id: req.params.id}, {text: text});
 
 		res.status(200).json(allTodos);
 	} catch (error) {
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const todoRoute = require('./routes/todo-route');
 var bodyParser = require('body-parser');
 require('dotenv').config();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 // mongoose connection
 var mongoDBURL =
@@ -25,4 +25,4 @@ app.use(bodyParser.json());
 app.use('/todo', todoRoute);
 
 
-server.listen(PORT, () => console.log('server is running on port 5000'));
+server.listen(PORT, () => console.log(`server is running on port ${PORT}`));
