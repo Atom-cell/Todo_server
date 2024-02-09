a1128d3c09dc97237ce87fef6206dfb413552011
sani.usnain <sani.usnain@onescreensolutions.com>
2024-02-09 14:14:37 +0500

    added reset password

--- routes/user-route.js
+++ routes/user-route.js
@@ -4,16 +4,16 @@ const User = require('../model/UserModel');
 const bcrypt = require('bcrypt');
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-	service: 'gmail',
-	auth: {
-		user: 'work.sani06@gmail.com',
-		pass: 'worc bmah wenp hlxq',
-	},
-	tls: {
-		rejectUnauthorized: false,
-	},
-});
+// const transporter = nodemailer.createTransport({
+// 	service: 'gmail',
+// 	auth: {
+// 		user: 'work.sani06@gmail.com',
+// 		pass: 'worc bmah wenp hlxq',
+// 	},
+// 	tls: {
+// 		rejectUnauthorized: false,
+// 	},
+// });
 
 const isValidPassword = async function (password, user) {
 	try {
@@ -23,6 +23,10 @@ const isValidPassword = async function (password, user) {
 	}
 };
 
+const hashPassword = async (password) => {
+	const salt = await bcrypt.genSalt(10);
+	return await bcrypt.hash(password, salt);
+}
 router.post('/register', async (req, res) => {
 	const { email, name, password, gender } = req.body;
 
@@ -42,10 +46,8 @@ router.post('/register', async (req, res) => {
 		if (existingUser) {
 			return res.status(400).json({ error: 'Email already taken' });
 		} else {
-			// Hash the password using bcrypt
-			const salt = await bcrypt.genSalt(10);
-			const hashedPassword = await bcrypt.hash(password, salt);
-
+			 
+			const hashedPassword = await hashPassword(password);
 			// Create a new user object
 			const user = new User({
 				email: email,
@@ -56,12 +58,10 @@ router.post('/register', async (req, res) => {
 
 			await user.save();
 
-			return res
-				.status(201)
-				.json({
-					message: 'Success',
-					data: { user_id: user._id, email: email, name: name },
-				});
+			return res.status(201).json({
+				message: 'Success',
+				data: { user_id: user._id, email: email, name: name },
+			});
 		}
 	} catch (err) {
 		return res.status(500).json({ error: 'Internal Server Error' });
@@ -94,29 +94,54 @@ router.post('/login', async (req, res) => {
 	}
 });
 
-router.get('/', async function (req, res) {
-	// send email
-	try {
-		let mailOptions = {
-			from: ' "Verify your email" <cinnakale@gmail.com>',
-			to: 'saniusnain@gmail.com',
-			subject: 'REMS - Email Verification',
-			html: `
-        <h2>  Thank you for choosing REMS </h2>
-        <h4>Please verify your email</h4>
-        <a>Verify your Email </a>
-        `,
-		};
+router.put('/reset-password', async (req, res) => {
+	const { email, password } = req.body;
 
-		transporter.sendMail(mailOptions, (err, info) => {
-			if (err) console.log(err.message);
-			else {
-				console.log('VERIFICATION EMAIL SENT!!!');
-			}
-		});
+	if (!email || !password) {
+		const field = !email ? 'email' : 'password';
+		return res.status(400).json({ error: `${field} is required` });
+	}
+
+	try {
+		const user = await User.findOne({ email: email });
+		if (user) {
+			const hashedPassword = await hashPassword(password);
+			await User.findOneAndUpdate({ email: email},{ password: hashedPassword});
+		} else {
+			return res.status(401).json({ error: 'Invalid email' });
+		}
+		return res
+			.status(201)
+			.json({ message: 'Successfully Reset'});
+
 	} catch (err) {
-		res.status(500).json({ message: err.message });
+		console.error(err);
+		return res.status(500).json({ error: 'Internal Server Error' });
 	}
 });
+// router.get('/', async function (req, res) {
+// 	// send email
+// 	try {
+// 		let mailOptions = {
+// 			from: ' "Verify your email" <cinnakale@gmail.com>',
+// 			to: 'saniusnain@gmail.com',
+// 			subject: 'REMS - Email Verification',
+// 			html: `
+//         <h2>  Thank you for choosing REMS </h2>
+//         <h4>Please verify your email</h4>
+//         <a>Verify your Email </a>
+//         `,
+// 		};
+
+// 		transporter.sendMail(mailOptions, (err, info) => {
+// 			if (err) console.log(err.message);
+// 			else {
+// 				console.log('VERIFICATION EMAIL SENT!!!');
+// 			}
+// 		});
+// 	} catch (err) {
+// 		res.status(500).json({ message: err.message });
+// 	}
+// });
 
 module.exports = router;
