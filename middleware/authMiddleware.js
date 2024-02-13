cd0fedca17ce561f292345a16f487b4a15f8315d
sani.usnain <sani.usnain@onescreensolutions.com>
2024-02-13 17:46:48 +0500

    duplicate login error added

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,8 +1,15 @@
 const jwt = require('jsonwebtoken');
-function verifyToken(req, res, next) {
+const RevokedToken = require('../model/RevokedTokenModel');
+
+async function verifyToken(req, res, next) {
 	const token = req.header('Authorization');
 	if (!token) return res.status(401).json({ error: 'Access denied' });
 	try {
+        const tokenRevoked = await RevokedToken
+        .findOne({access_token: token});
+        if(tokenRevoked) {
+            return res.status(401).json({ error: 'Duplicate Access not allowed' });
+        }
 		const decoded = jwt.verify(token, 'your-secret-key');
 		req.userId = decoded.userId;
 		next();
