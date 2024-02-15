783a99c86f7e28b35194059d0a03cb61454a2eae
sani.usnain <sani.usnain@onescreensolutions.com>
2024-02-15 11:47:49 +0500

    added edit route

--- routes/todo-route.js
+++ routes/todo-route.js
@@ -9,9 +9,16 @@ router.post('/', verifyToken, async (req, res, next) => {
 		if (!text) {
 			return res.status(400).json({ error: 'Text is required for a todo.' });
 		}
-		const newTodo = new Todo({ text:text, type:type, description:description, user_id: req.userId });
+		const newTodo = new Todo({
+			text: text,
+			type: type,
+			description: description,
+			user_id: req.userId,
+		});
 		await newTodo.save();
-		res.status(201).json({ message: 'Todo added successfully.', todo: newTodo });
+		res
+			.status(201)
+			.json({ message: 'Todo added successfully.', todo: newTodo });
 	} catch (error) {
 		console.error(error);
 		res.status(500).json({ error: 'Internal Server Error' });
@@ -23,8 +30,8 @@ router.get('/:completed/', verifyToken, async (req, res) => {
 	const completed = req.params.completed;
 	const type = req.query.type;
 	try {
-		const filter = { user_id: userId, completed: completed}
-		if(type) filter['type'] = type;
+		const filter = { user_id: userId, completed: completed };
+		if (type) filter['type'] = type;
 
 		const allTodos = await Todo.find(filter).sort({ _id: -1 });
 		res.status(200).json(allTodos);
@@ -46,12 +53,21 @@ router.delete('/:id', verifyToken, async (req, res) => {
 
 router.put('/:id', verifyToken, async (req, res) => {
 	try {
-		const { text } = req.body;
-		const allTodos = await Todo.findOneAndUpdate(
+		const { text, description, type } = req.body;
+		if (!text || !type) {
+			const field = !text ? 'Text' : !type ? 'Type' : null;
+			return res
+				.status(400)
+				.json({ error: `${field} is required for a todo.` });
+		}
+		const editTodo = await Todo.findOneAndUpdate(
 			{ _id: req.params.id },
-			{ text: text }
+			{ text: text, type: type, description: description },
+			{ new: true }
 		);
-		res.status(200).json(allTodos);
+		res
+			.status(200)
+			.json({ message: 'Todo edited successfully.', todo: editTodo });
 	} catch (error) {
 		console.error(error);
 		res.status(500).json({ error: 'Internal Server Error' });
