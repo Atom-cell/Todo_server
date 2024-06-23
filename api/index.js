594150e68d1188b3871e629e7aeea0ae038e3ae9
Atom-cell <rajpootsaniusnain@gmail.com>
2024-06-23 13:06:37 +0500

    fixed env error

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const http = require('http');
 const app = express();
@@ -7,17 +8,18 @@ const mongoose = require('mongoose');
 const todoRoute = require('../routes/todo-route');
 const userRoute = require('../routes/user-route');
 var bodyParser = require('body-parser');
-require('dotenv').config();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 // mongoose connection
-var mongoDBURL =
-	`mongodb+srv://sani:${process.env.DB_PASSWORD}@cluster0.jcplaj0.mongodb.net/todos?retryWrites=true&w=majority`;
-try {
-	mongoose.connect(mongoDBURL);
-} catch (e) {
-	console.error("Couldn't connect to MongoDB");
-}
+var mongoDBURL = `mongodb+srv://sani:${process.env.DB_PASSWORD}@cluster0.jcplaj0.mongodb.net/todos?retryWrites=true&w=majority`;
+mongoose
+	.connect(mongoDBURL)
+	.then(() => {
+		console.log('Connected to DB');
+	})
+	.catch((error) => {
+		console.error('MongoDB connection error:', error);
+	});
 
 app.use(cors());
 // parse application/json
@@ -27,11 +29,8 @@ app.use('/todo', todoRoute);
 
 app.use('/user', userRoute);
 
-app.use('/', function(req, res) {
-	res.json({message: "Base route"})
+app.use('/', function (req, res) {
+	res.json({ message: 'Base route' });
 });
-
-
-
 
 server.listen(PORT, () => console.log(`server is running on port ${PORT}`));
